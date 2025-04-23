6b5cbb5c1265e86d4da3610ee3b4e3d6c2c109fe
Laly <laly.teissier@gmail.com>
2025-04-23 21:03:07 +0200

    feat: add constants file

---
+++ src/constants.js
@@ -0,0 +1,28 @@
+// A REFAIRE pour simplifier - mettre en MAJ
+export const POSITIONS_ZOOM = {
+    'monde-medieval': [35, 0, 4],
+    'monde-victorien': [65, 0, 4],
+    'monde-guerre': [95, 0, 4],
+}
+export const POSITIONS_PARCHEMIN = {
+    'monde-medieval': [35, 0, 0],
+    'monde-victorien': [65, 0, 0],
+    'monde-guerre': [95, 0, 0],
+}
+
+export const SETTINGS = {
+    DEBUG: true,
+    SCROLL_SPEED: 0.04, 
+    MAX_TRANSLATION: 100
+}
+
+export const TEXTS = {
+    INTRO: 'blablabla blabla'
+}
+
+export const ASSETS = {
+    TEXTURE_PARCHEMIN: '/texture_parchemin.png',
+    WAR_FRAME: '/cadre_1942.png' 
+}
+
+export default { POSITIONS_ZOOM, POSITIONS_PARCHEMIN, SETTINGS, ASSETS }
