93a1b6934ce118c3fca92e296769aff736f64ef7
Laly <laly.teissier@gmail.com>
2025-05-11 22:31:20 +0200

    fix: simplify

--- src/componants/ArrowButton.jsx
+++ src/componants/ArrowButton.jsx
@@ -1,15 +1,7 @@
 import { useTexture } from '@react-three/drei'
-import { Vector3 } from 'three'
-
-import useVoiceOverStore from '../stores/useVoiceOverStore'
-import useZoom from '../hooks/useZoom'
-import { POSITIONS_PARCHEMIN } from '../constants'
 
 export default function ArrowButton({ position = [1, -5, 0], onClick = () => {}, ...props }) {
-
-    const texture = useTexture('./arrow.png')
-    
-    const toggleZoom = useZoom(POSITIONS_PARCHEMIN[0], new Vector3(0, 0, 0)) // hook de zoom
+    const texture = useTexture('./arrow.png') // TODO : remplacer par le bon fichier
 
     return (
         <mesh
