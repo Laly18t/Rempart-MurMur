e6f58657c8aacf1409898a9344a63d8308ff70c6
Laly <laly.teissier@gmail.com>
2025-05-12 17:06:06 +0200

    fix: preparation for useStore

--- src/componants/UI/SubtitleButton.jsx
+++ src/componants/UI/SubtitleButton.jsx
@@ -5,20 +5,20 @@ import useVoiceOverStore from '../../stores/useVoiceOverStore'
 export default function SubtitleButton() {
     // TODO: add subtitle gestion on store
     
-    // const { mute, setMute } = useVoiceOverStore()
+    // const { subtitle, setsubtitle } = useVoiceOverStore()
 
     const texture = ('./subtitle.png')
-    // const texture = !mute ? textures.on : textures.off
+    // const texture = !subtitle ? textures.on : textures.off
 
     return (
-        <div className='subtitleButton'>
+        <button className='subtitleButton'>
             <img
                 src={texture}
                 alt="subtitle"
                 onClick={() => {
-                    // setMute(!mute)
+                    // setsubtitle(!subtitle)
                 }}
             />
-        </div>
+        </button>
     )
 }
