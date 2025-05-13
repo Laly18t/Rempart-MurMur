7a480f3ccda2ca0b60c3c97df4770cc6aeb818f1
Laly <laly.teissier@gmail.com>
2025-05-13 10:16:35 +0200

    feat: missig file for get info about scene

--- src/stores/useSceneStore.js
+++ src/stores/useSceneStore.js
@@ -1,8 +1,30 @@
 import { create } from 'zustand'
 
-const useSceneStore = create((set) => ({
+const useSceneStore = create((set, get) => ({
     currentScene: null, // default scene
     cameraTarget: null, // position cible pour la caméra (Vector3)
+    scenesGroups: {},
+    cameraScenes: [],
+
+    setSceneInfo: (sceneName, { group, cameras }) =>
+        set((state) => ({
+            scenesGroups: {
+                ...state.scenesGroups,
+                [sceneName]: group,
+            },
+            cameraScenes: {
+                ...state.cameraScenes,
+                [sceneName]: cameras,
+            },
+        })),
+    
+    getCurrentSceneInfo: () => {
+        const { currentScene, scenesGroups, cameraScenes } = get()
+        return {
+            group: scenesGroups[currentScene],
+            cameras: cameraScenes[currentScene],
+        }
+    },
 
     setCurrentScene: (scene, position = null) =>
         set(() => ({
