e9736d43b7a3587de59c195f25be6b4650fca6e9
Laly <laly.teissier@gmail.com>
2025-05-29 21:58:25 +0200

    feat: add lotie for loader

--- src/componants/UI/Loader.jsx
+++ src/componants/UI/Loader.jsx
@@ -1,10 +1,22 @@
 import { useProgress } from '@react-three/drei'
 import { useState, useEffect } from 'react'
+import Lottie from 'react-lottie'
+
 import { TEXTS } from '../../constants'
+import animationData from '../../lotties/loader_v2.json'
 
 export default function Loader({ onFinish }) {
     const { progress } = useProgress()
     const [visible, setVisible] = useState(true)
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        play: false,
+        animationData: animationData,
+        rendererSettings: {
+            preserveAspectRatio: "xMidYMid slice"
+        }
+    }
 
     useEffect(() => {
         if (progress === 100 && visible) {
@@ -27,14 +39,13 @@ export default function Loader({ onFinish }) {
             transform: 'translate(-50%, -50%)',
             color: 'white',
             fontSize: '1.5em',
-            background: 'rgba(0, 0, 0, 1)',
-            padding: '1em 2em',
+            // background: 'rgba(0, 0, 0, 1)',
+            padding: '0.5em 1em',
             borderRadius: '10px',
             pointerEvents: 'none'
         }}>
+            <Lottie animationData={animationData} options={defaultOptions} style={{ background: 'transparent', width: '190%', height:'190%', transform: 'translate(-25%, 0%)', }} />
             <p style={{textAlign:'center'}}>{progress.toFixed(0)} % chargé </p>
-
-            <p>{TEXTS.LOADER}</p>
         </div>
     )
 }
