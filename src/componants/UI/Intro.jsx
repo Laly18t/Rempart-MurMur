7a0d868e3cf28b125110e8cda286ceda18456fe2
Laly <laly.teissier@gmail.com>
2025-05-30 19:19:23 +0200

    feat: add intro video in fullscreen

--- src/componants/UI/Intro.jsx
+++ src/componants/UI/Intro.jsx
@@ -1,35 +1,49 @@
-import { Text } from "@react-three/drei"
-import { useLoader } from "@react-three/fiber"
+import { Html, Text, useVideoTexture } from "@react-three/drei"
+import { useLoader, useThree } from "@react-three/fiber"
 import { TextureLoader } from "three"
 
 import ArrowButton from "../ArrowButton"
 import useAppStore from "../../stores/useAppStore"
 import IntroAnimation from "../animations/IntroAnimation"
 import useVoiceOverStore from "../../stores/useVoiceOverStore"
-import { useEffect } from "react"
+import { useEffect, useRef } from "react"
 import usePlaySound from "../../hooks/usePlaySound"
+import videoSrc from '/ui/intro_v1.mp4'
 
 export default function Intro({ debug = false, ...props }) {
     const step = useAppStore((state) => state.step)
     const nextStep = useAppStore((state) => state.nextStep)
     const voiceOver = useVoiceOverStore()
+    const videoRef = useRef(null)
+    const { viewport } = useThree()
+    console.log(viewport)
+
     const handleClickButton = () => {
         nextStep()
     }
 
+    const videoTexture = useVideoTexture(
+        videoSrc,
+        {
+            muted: false,
+            loop: false,
+            start: step === 2
+        }
+    )
+
     const texture = useLoader(TextureLoader, './castle_clean.PNG')
     const playSound = usePlaySound(`/audio/sounds/parchemin_v1_1.wav`)
-    
+
     // play de la musique de fond
     useEffect(() => {
-        if(step === 2){
-            playSound.play()
+        if (step === 2) {
+            // playSound.play()
             console.log('play', step)
-        } else{
+        } else {
             playSound.stop()
         }
     }
-    , [step])
+        , [step])
 
     return <>
         {/* bouton pour le son - TODO: refonte graphique */}
@@ -39,10 +53,27 @@ export default function Intro({ debug = false, ...props }) {
                 <meshBasicMaterial color={"limegreen"} />
             </mesh>}
 
-            <mesh position={[0,0.1,0]}> {/* TODO: temporaire */}
-                <planeGeometry args={[6.3, 2.8]} /> 
+            {step === 2 && (
+                <mesh position={[0, 0, -1]}>
+                    <planeGeometry args={[viewport.width + 1.6, viewport.height + 1]} />
+                    <meshBasicMaterial map={videoTexture} />
+                </mesh>
+            )}
+            {/* <mesh position={[0, 0.1, 0]}> 
+                <planeGeometry args={[6.3, 2.8]} />
                 <meshBasicMaterial map={texture} transparent={true} />
-            </mesh>
+            </mesh> */}
+            {/* {step === 2 && (
+            <Html position={[-2, 2, 0]} fullscreen>
+                <video ref={videoRef} autoPlay preload="auto" playsInline className="fullscreen-video" >
+                    <source
+                        src={videoSrc}
+                        type="video/mp4"
+                    />
+                    Votre navigateur ne supporte pas la balise vidéo.
+                </video>
+            </Html>
+            )} */}
 
             {/* TODO: avoir la bonne animation */}
             {/* {step > 1 && <IntroAnimation />} */}
