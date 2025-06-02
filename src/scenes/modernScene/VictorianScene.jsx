6e14f60df3abe2ee054a49edbbc69b7356c45da7
Laly <laly.teissier@gmail.com>
2025-06-02 15:56:47 +0200

    fix: opti and good outline

--- src/scenes/modernScene/VictorianScene.jsx
+++ src/scenes/modernScene/VictorianScene.jsx
@@ -1,6 +1,6 @@
 import React, { useRef, useEffect, useMemo, forwardRef, useState } from 'react'
 import { useGLTF, PerspectiveCamera, Outlines } from '@react-three/drei'
-import { AnimationMixer, LoopOnce, MeshNormalMaterial, TextureLoader } from 'three'
+import { AnimationMixer, LoopOnce, MeshBasicMaterial, MeshNormalMaterial, TextureLoader } from 'three'
 import { useFrame, useLoader, useThree } from '@react-three/fiber'
 
 import useSceneStore from '../../stores/useSceneStore'
@@ -11,23 +11,26 @@ import usePlaySound from '../../hooks/usePlaySound'
 import useFrameAnimation from '../../hooks/useFrameAnimation'
 
 function VictorianScene({ ...props }, ref) {
-    const { animations, scene } = useGLTF('/models/scene_1697_V2.glb') // load model
-    const groupRef = ref ?? useRef()
+    const { animations, scene } = useGLTF('/models/scene_1697_v4.glb') // load model
     const { set, gl, camera } = useThree()
 
     const voiceOver = useVoiceOverStore()
     const { isSceneFinished } = useVoiceOverStore()
-    const { currentScene } = useSceneStore()
+    const { currentScene, isZoom, setIsZoom } = useSceneStore()
 
+    const groupRef = ref ?? useRef()
     const mixers = useRef([])
     const cameraRefs = useRef({}) // Pour stocker toutes les caméras
     const salleRef = useRef()
     const salleDRef = useRef()
     const bookRef = useRef()
     const flowerRef = useRef()
-    const [showBookOutline, setShowBookOutline] = useState(false)
-    const [showFlowerOutline, setShowFlowerOutline] = useState(false)
 
+    const [showBookOutline, setShowBookOutline] = useState(true)
+    const [showFlowerOutline, setShowFlowerOutline] = useState(true)
+    const [showPeople, setShowPeople] = useState(true)
+
+    const backButton = useLoader(TextureLoader, "/ui/icons/fleche_gauche.svg")
     const peopleFrames = [
         '/animations/modern/1697_1.png',
         '/animations/modern/1697_2.png',
@@ -42,23 +45,40 @@ function VictorianScene({ ...props }, ref) {
     } = useFrameAnimation(peopleFrames, 0.5, true, true)
 
     const playBook = usePlaySound('/audio/sounds/book.mp3')
-    const playFlower = usePlaySound('/audio/sounds/bouquet_v1.mp3')
+    const playFlower = usePlaySound('/audio/sounds/bouquet.mp3')
     const playFire = usePlaySound('/audio/sounds/incendie_v1.mp3')
 
     // gestion des cameras
     useEffect(() => {
         scene.traverse((child) => {
-            // console.log('child', child.name)
-            if (!child.isMesh) {
-                console.log('child', child.name, child.isMesh)
+            // console.log('child -->', child.name)
+            if (child.isMesh) {
+                const oldMaterial = child.material
+
+                // On récupère la texture map de l'ancien matériau
+                const bakedTexture = oldMaterial.map
+
+                child.material = new MeshBasicMaterial({
+                    map: bakedTexture,
+                    transparent: oldMaterial.transparent,
+                    opacity: oldMaterial.opacity,
+                })
+
+                if (child.name === "1697_livre_ouvert") {
+                    bookRef.current = child
+                    setShowBookOutline(true)
+                }
+
+                if (child.name === "1697_bouquet_mot") {
+                    flowerRef.current = child
+                    setShowFlowerOutline(true)
+                }
             }
-            if (child.isMesh && child.name === "livre_ouverture") {
-                bookRef.current = child
-                setShowBookOutline(true)
+            if (child.name === "bake") {
+                salleRef.current = child
             }
-            if (child.isMesh && child.name === "1697_groupe61") {
-                flowerRef.current = child
-                setShowFlowerOutline(true)
+            if (child.name === "bake_noir") {
+                salleDRef.current = child
             }
 
             if (child.name === 'cam_ensemble') { //Camera_face
@@ -85,38 +105,42 @@ function VictorianScene({ ...props }, ref) {
         console.log('Objet cliqué:', clickedObject.name)
 
         // action 1 - ouvrir le livre
-        if (clickedObject.name === 'dessus') {
-            console.log('livre cliqué', clickedObject)
-            setShowBookOutline(false)
-            console.log('cameraRefs', cameraRefs.current.camera2)
+        if (clickedObject.name === '1697_livre_ouvert') {
+            // console.log('livre cliqué', clickedObject)
+            if (isZoom) {
+                console.log('déjà zoomé sur le livre')
+                handleZoom()
+            } else {
+                setIsZoom(true)
+                cameraZoom(
+                    camera,
+                    cameraRefs.current.camera2,
+                    () => {
+                        voiceOver.setIndex(1)
 
-            cameraZoom(
-                camera,
-                cameraRefs.current.camera2,
-                () => {
-                    voiceOver.setIndex(1)
+                        const clip = animations.find(a => a.name === 'animation_0')
+                        const mixer = new AnimationMixer(scene)
+                        const action = mixer.clipAction(clip)
 
-                    const clip = animations.find(a => a.name === 'animation_0')
-                    const mixer = new AnimationMixer(scene)
-                    const action = mixer.clipAction(clip)
+                        if (clip) {
+                            action.setLoop(LoopOnce, 1)
+                            action.clampWhenFinished = true
+                            action.reset().play()
 
-                    if (clip) {
-                        action.setLoop(LoopOnce, 1)
-                        action.clampWhenFinished = true
-                        action.reset().play()
+                            playBook.play() // bruitage livre
 
-                        playBook.play() // bruitage livre
-
-                        // Ajouter le mixer pour mise à jour via useFrame
-                        mixers.current.push({ mixer, action })
-                    }
-                },
-                props.portalGroupRef.current
-            )
+                            // Ajouter le mixer pour mise à jour via useFrame
+                            mixers.current.push({ mixer, action })
+                        }
+                    },
+                    props.portalGroupRef.current
+                )
+            }
         }
 
         // action 2 - voir le bouquet
-        if (clickedObject.name === '1697_groupe61') {
+        if (clickedObject.name === '1697_bouquet_mot') {
+            setIsZoom(true)
             console.log('bouquet cliqué')
             setShowFlowerOutline(false)
 
@@ -137,69 +161,72 @@ function VictorianScene({ ...props }, ref) {
         if (salleRef.current && salleDRef.current) {
             if (currentScene === "monde-moderne" && isSceneFinished) {
                 console.log("switch", salleRef.current.visible)
+                setShowFlowerOutline(false)
+                setShowBookOutline(false)
+                setShowPeople(false)
                 salleRef.current.visible = false
                 salleDRef.current.visible = true
                 playFire.play() // bruitage incendie
             }
         }
-    }, [currentScene, isSceneFinished])
+    }, [currentScene, isSceneFinished, showPeople])
     useEffect(() => {
-        console.log("scene -->", scene)
-        if (scene) {
-            salleRef.current = scene.children[1]
-            salleDRef.current = scene.children[0]
+        if (scene && salleRef.current && salleDRef.current) {
             salleRef.current.visible = true
             salleDRef.current.visible = false
-
-            console.log("salleRef", salleRef.current.children)
-
         }
     }, [scene])
 
+    const handleZoom = () => {
+        cameraZoom(
+            camera,
+            cameraRefs.current.camera1,
+            props.portalGroupRef.current,
+        )
+        setIsZoom(false)
+    }
 
     return <>
         <group position={[0, -2, -1]} rotation-y={-3.14} ref={groupRef} {...props} dispose={null} onClick={handleClick}>
 
-            {/* Outline pour le lustre */}
-            {bookRef.current && (
-                <>
-                    <primitive castShadow receiveShadow object={bookRef.current}>
-                        <Outlines
-                            visible={showBookOutline}
-                            color="white"
-                            thickness={8}
-                            opacity={1}
-                            transparent={false}
-                            angle={Math.PI}
-                        />
-                    </primitive>
-                </>
-            )}
-            {/* Outline pour le lustre */}
-            {flowerRef.current && (
-                <>
-                    <primitive castShadow receiveShadow object={flowerRef.current}>
-                        <Outlines
-                            visible={showFlowerOutline}
-                            color="white"
-                            thickness={8}
-                            opacity={1}
-                            transparent={false}
-                            angle={Math.PI}
-                        />
-                    </primitive>
-                </>
-            )}
+            {/* Outline pour le livre */}
+            {bookRef.current && (<>
+                <primitive object={bookRef.current}>
+                    <Outlines
+                        visible={showBookOutline}
+                        color="white"
+                        thickness={4}
+                        opacity={1}
+                        transparent={false}
+                        angle={Math.PI}
+                    />
+                </primitive>
+            </>)}
 
-            <mesh position={[-2.7, 1.1, -1.9]} rotation-y={-3}> {/* TODO: temporaire */}
+            {/* Outline pour le bouquet */}
+            {flowerRef.current && (<>
+                <primitive object={flowerRef.current}>
+                    <Outlines
+                        visible={showFlowerOutline}
+                        color="white"
+                        thickness={2}
+                        opacity={1}
+                        transparent={false}
+                        angle={Math.PI}
+                    />
+                </primitive>
+            </>)}
+
+            <mesh visible={showPeople} position={[-2.7, 1.1, -1.9]} rotation-y={-3}> {/* TODO: temporaire */}
                 <boxGeometry args={[1.4, 1.8, 0.00001]} />
                 <meshBasicMaterial map={animatedPeopleTexture} transparent={true} />
                 {/* <meshBasicMaterial color='red' /> */}
             </mesh>
 
-            <primitive castShadow receiveShadow object={scene} />
-            <ambientLight intensity={1.5} />
-            <spotLight position={[0, 5, 5]} intensity={0.8} />
+            <primitive castShadow receiveShadow object={scene} renderOrder={1} />
+
+            {/* <ambientLight intensity={2} />
+            <spotLight position={[0, 5, 5]} intensity={0.8} /> */}
 
             {currentScene === 'monde-moderne' &&
                 <>
@@ -215,10 +242,21 @@ function VictorianScene({ ...props }, ref) {
                     />
                 </>
             }
+
+            {isZoom && (<>
+                <mesh position={[1.2, 3.2, 1]} onClick={handleZoom}>
+                    <boxGeometry args={[0.3, 0.3, 0.00001]} />
+                    <meshBasicMaterial transparent map={backButton} />
+                </mesh>
+                <mesh position={[0.3, 1.25, -3]} rotation-y={Math.PI} rotation-x={Math.PI / 2} onClick={handleZoom}>
+                    <boxGeometry args={[0.1, 0.1, 0.00001]} />
+                    <meshBasicMaterial transparent color='red' />
+                </mesh>
+            </>)}
         </group>
     </>
 }
 
 export default forwardRef(VictorianScene)
 
-useGLTF.preload('/models/scene_1697_V2.glb')
+useGLTF.preload('/models/scene_1697_v4.glb')
