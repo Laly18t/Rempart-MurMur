01b6a51560099ff48b250882d1a7a4fb85919df9
Laly <laly.teissier@gmail.com>
2025-06-02 23:06:03 +0200

    feat: add vidoe full screen

--- src/componants/UI/Outro.jsx
+++ src/componants/UI/Outro.jsx
@@ -1,33 +1,29 @@
-import { Text } from "@react-three/drei"
-import useAppStore from "../../stores/useAppStore"
-import { useLoader } from "@react-three/fiber"
-import { TextureLoader } from "three"
-import ArrowButton from "../ArrowButton"
+import { Text, useVideoTexture } from "@react-three/drei"
+import { useLoader, useThree } from "@react-three/fiber"
 import { useState, useEffect } from "react"
+
+import useAppStore from "../../stores/useAppStore"
+import ArrowButton from "../ArrowButton"
+import videoSrc from '/animations/intro.webm'
+import { TextureLoader } from "three"
 
 export default function Conclusion({ debug = false, ...props }) {
     const step = useAppStore((state) => state.step)
     const setStep = useAppStore((state) => state.setStep)
-    const nextStep = useAppStore((state) => state.nextStep)
-
-    const [textureIndex, setTextureIndex] = useState(0) // 0: clean, 1: dammage, 2: final
+    const { viewport } = useThree()
+    
     const [showCTA, setShowCTA] = useState(false)
-
-    const textures = useLoader(TextureLoader, [
-        './castle_clean.PNG',
-        './castle_dammage.PNG',
-        './castle_renovation.PNG'
-    ])
+    const [hovered, setHovered] = useState(false)
     const textureButton = useLoader(TextureLoader, './ui/icons/cta_background_defaut.png')
 
-    const handleMeshClick = () => {
-        // Toggle entre 0 et 1 uniquement
-        setTextureIndex(prev => prev === 0 ? 1 : 0)
-    }
-
-    const handleCTAClick = () => {
-        setTextureIndex(2)
-    }
+    const videoTexture = useVideoTexture(
+            videoSrc,
+            {
+                muted: true,
+                loop: false,
+                start: step === 7
+            }
+        )
 
     const handleReturnButton = () => {
         setStep(step - 1)
@@ -37,28 +33,27 @@ export default function Conclusion({ debug = false, ...props }) {
     useEffect(() => {
         const timer = setTimeout(() => {
             setShowCTA(true)
-        }, 35000) // 35000
+        }, 55000) // 35000
 
         return () => clearTimeout(timer)
     }, [])
 
+    useEffect(() => {
+        if (hovered) {
+            console.log("Hover -->")
+        } 
+    }, [hovered])
+
     return (
         <group {...props}>
-            {debug && (
-                <mesh position={[0, 0, 0]}>
-                    <planeGeometry args={[28, 18]} />
-                    <meshBasicMaterial color={"limegreen"} />
-                </mesh>
-            )}
 
             {/* Mesh interactif */}
-            <mesh position={[0, 0.4, 0]} onClick={handleMeshClick}>
-                <planeGeometry args={[6.3, 2.8]} />
-                <meshBasicMaterial
-                    map={textures[textureIndex]}
-                    transparent={true}
-                />
-            </mesh>
+            {step === 7 && (
+                <mesh position={[0, 0, -1]}>
+                    <planeGeometry args={[viewport.width + 1.6, viewport.height + 1]} />
+                    <meshBasicMaterial transparent={true} map={videoTexture} />
+                </mesh>
+            )}
 
             {/* Titre principal */}
             <Text
@@ -74,7 +69,12 @@ export default function Conclusion({ debug = false, ...props }) {
 
             {/* CTA de fin visible après 35s */}
             {showCTA && (
-                <mesh position={[0, -1.3, 0]}>
+                <mesh 
+                    position={[0, -1.3, 0]}
+                    onPointerEnter={() => setHovered(true)}
+                    onPointerLeave={() => setHovered(false)}
+                    scale={hovered ? [1.1, 1.1, 1.1] : [1, 1, 1]}
+                >
                     <planeGeometry args={[2, 0.5]} />
                     <meshBasicMaterial map={textureButton} transparent={true} />
                     <Text
@@ -84,7 +84,6 @@ export default function Conclusion({ debug = false, ...props }) {
                         anchorY="center"
                         anchorX="center"
                         lineHeight={0.8}
-                        onClick={handleCTAClick}
                         style={{ cursor: 'pointer', textTransform: 'uppercase', }}
                     >
                         Participer
