10ab7b87487321faf24ac9687e0f43598c1688bb
Laly <laly.teissier@gmail.com>
2025-06-04 00:53:47 +0200

    fix: update frame

--- src/constants.js
+++ src/constants.js
@@ -51,7 +51,7 @@ export const SETTINGS = {
 export const ASSETS = {
     TEXTURE_PARCHEMIN: '/texture_parchemin.png',
     WAR_FRAME: '/cadre_1942.webp',
-    MODERN_FRAME: '/cadre_1789.webp',
+    MODERN_FRAME: '/cadre_1697.webp',
     MEDIEVAL_FRAME: '/cadre_1317.webp',
     MEDIEVAL_BADGE: '/badge_1317.webp',
     MODERN_BADGE: '/badge_1789.webp',
